339962dc86a7c6ce6e54abdd545ddd8571b01336
emrahburak <gemrahburak@gmail.com>
2022-03-26 20:28:24 +0300

    ...

--- components/Card/index.js
+++ components/Card/index.js
@@ -1,14 +1,38 @@
+import styles from './Card.module.scss';
 
 
 const Card = () => {
-    return (
-        <article className="card">
-            card
+  return (
+    <article className="card">
+      <div>
+        <p>Name</p>
+        <a>up</a>
+        <a>down</a>
+        <a>number</a>
+      </div>
+      <div>
+        I'm baby heirloom drinking vinegar pop-up ramps. Raw denim irony hella
+        palo santo. Franzen hashtag edison bulb pork belly, tote bag chia
+        everyday carry microdosing. Kinfolk lyft literally, plaid kale chips
+        ramps drinking vinegar normcore kombucha etsy subway tile before they
+        sold out sustainable chia pop-up.
+      </div>
 
-        </article>
-
-    )
-}
-
+      <div>
+        Bespoke lomo kombucha flexitarian tote bag brooklyn retro schlitz shabby
+        chic swag meditation hoodie vexillologist messenger bag pork belly. Palo
+        santo XOXO austin poke live-edge offal kickstarter irony. Flannel
+        organic YOLO tattooed. Hashtag fingerstache portland before they sold
+        out locavore photo booth, heirloom pour-over. Farm-to-table pickled four
+        dollar toast succulents deep v marfa hot chicken forage flannel
+        chillwave ethical pitchfork. Tilde lo-fi mustache irony shaman meh
+        brooklyn hell of readymade occupy try-hard. Pinterest thundercats
+        stumptown coloring book retro authentic hell of try-hard pabst swag
+        fanny pack pitchfork pork belly. Fashion axe kitsch mlkshk, tofu
+        gentrify YOLO drinking vinegar.
+      </div>
+    </article>
+  );
+};
 
 export default Card;
--- components/LeftSidebar/index.js
+++ components/LeftSidebar/index.js
@@ -4,7 +4,7 @@ import Card from '../Card';
 const LeftSidBar = () => {
     return(
         <div >
-            left-sidebar
+            Kim Ne dio?
             <Card/>
             <Card/>
             <Card/>
